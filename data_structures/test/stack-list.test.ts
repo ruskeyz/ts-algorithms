d83fb1b64b63f2f4035f6b26c0a8a965120d6bfc
ruskeyz <lutayilya@gmail.com>
2024-04-19 18:44:53 +0100

    stack ds

---
+++ data_structures/test/stack-list.test.ts
@@ -0,0 +1,22 @@
+import Stack from "../stack-list";
+
+describe("Stack", () => {
+  test("constructor", () => {
+    const s = new Stack();
+    expect(s instanceof Stack);
+  });
+
+  test("push()", () => {
+    const s = new Stack();
+    expect(s.push(100)).toBe(1);
+    expect(s.push(200)).toBe(2);
+  });
+
+  test("pop()", () => {
+    const s = new Stack();
+    s.push(100);
+    s.push(200);
+    expect(s.pop()).toBe(200);
+    expect(s.pop()).toBe(100);
+  });
+});
