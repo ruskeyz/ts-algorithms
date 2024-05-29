d5710959cd8ec1ea6742d3bd6fb3df6fcf4a8211
ruskeyz <lutayilya@gmail.com>
2024-05-29 22:18:03 +0100

    heap test

---
+++ data_structures/test/heap.test.ts
@@ -0,0 +1,24 @@
+import MinHeap from "../heap";
+
+describe("Heap", () => {
+  test("Heap insert", () => {
+    const h = new MinHeap();
+    expect(h.length).toEqual(0);
+    h.insert(5);
+    h.insert(3);
+    h.insert(69);
+    h.insert(420);
+    h.insert(4);
+    h.insert(1);
+    h.insert(8);
+    h.insert(7);
+    expect(h.length).toBe(8);
+    expect(h.delete()).toEqual(1);
+    let i = 7;
+    while (i > 0) {
+      h.delete();
+      i--;
+    }
+    expect(h.length).toBe(0);
+  });
+});
