39c789e5b4941467f8a08f547717471dcb314a2d
ruskeyz <lutayilya@gmail.com>
2024-06-19 17:32:35 +0100

    binary search tree validate method

--- data_structures/binary-search-tree.ts
+++ data_structures/binary-search-tree.ts
@@ -84,6 +84,7 @@ export default class BinarySearchTree {
       this.insertNode(this.head, newNode);
     }
   }
+
   private deleteNode(
     current: BinaryNode<number> | null,
     value: number,
@@ -127,6 +128,33 @@ export default class BinarySearchTree {
   //inOrder traversal
   traverse(): number[] {
     return this.walkTree(this.head, []);
+  }
+  private validateNode(
+    current: BinaryNode<number> | null,
+    s: number,
+    b: number,
+  ) {
+    // base
+    if (!current) {
+      return true;
+    }
+    let v = current.value;
+    if (v <= s || v >= b) {
+      return false;
+    }
+    const leftOk = this.validateNode(current.left, s, current.value);
+    const rightOk = this.validateNode(current.right, current.value, b);
+
+    return leftOk && rightOk;
+  }
+  validate(): boolean {
+    const s = -Infinity;
+    const b = Infinity;
+    if (!this.head) {
+      return false;
+    } else {
+      return this.validateNode(this.head, s, b);
+    }
   }
 }
 
---
+++ data_structures/test/binary-search-tree.test.ts
@@ -0,0 +1,18 @@
+import BinaryNode from "../binary-node";
+import BinarySearchTree from "../binary-search-tree";
+
+describe("Binary Search Tree", () => {
+  test("It validates the tree", () => {
+    const tree = new BinarySearchTree();
+    tree.head = new BinaryNode(10);
+    expect(tree.validate()).toBeTruthy();
+    tree.head.left = new BinaryNode(7);
+    expect(tree.validate()).toBeTruthy();
+    tree.head.right = new BinaryNode(14);
+    expect(tree.validate()).toBeTruthy();
+    tree.head.right.right = new BinaryNode(20);
+    expect(tree.validate()).toBeTruthy();
+    tree.head.right.left = new BinaryNode(9);
+    expect(tree.validate()).toBeFalsy();
+  });
+});
